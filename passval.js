a7ceac8d59990df14b4bdf65c4ee93324f3df65d
anak bontang <fatkurrizal12@gmail.com>
2021-11-01 15:45:57 +0700

    pencerahan

--- passval.js
+++ passval.js
@@ -2,13 +2,19 @@ const passing = () => {
     var fullnames = document.getElementById("fullname").value;
     var emails = document.getElementById("email").value;
     var phones = document.getElementById("phone").value;
-    var nationalitys = document.getElementById("nationality").value;
+    var nationalitys = document.getElementById("nasionality").value;
     var messages = document.getElementById("message").value;
     localStorage.setItem("vnama", fullnames);
     localStorage.setItem("vemail", emails);
     localStorage.setItem("vphone", phones);
     localStorage.setItem("vnationality", nationalitys);
     localStorage.setItem("vmessage", messages);
+
+    console.log("testt passing");
+
     return false;
   
+    
   };
+
+  console.log("testt diluar passing");
