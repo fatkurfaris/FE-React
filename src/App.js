36010c5d367a0e380a5a146992550f7f47947a5b
anak bontang <fatkurrizal12@gmail.com>
2021-11-04 11:46:07 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [todoList, setTodoList] = useState([])
+  const [form, setForm] = useState ({
+    todo: '',
+    status : false
+  })
+
+  const resetInput =() => {
+    setForm({
+      todo: '',
+      status: false
+    })
+  }
+
+  const handleChange =(e) => {
+    setForm({
+      ...form,
+      todo: e.target.value,
+      status: false
+    })
+  }
+
+  const handleSubmit =(e) => {
+    e.preventDefault();
+    console.log(form.index);
+    if(form.index || form.index===0){
+      const newTodo = todoList.map((e,i)=>{
+        if(i===form.index){
+          return form
+        }else{
+          return e
+        }
+      })
+      setTodoList(newTodo)
+    }else{
+      setTodoList([
+        ...todoList,
+        form
+      ])
+    }
+    resetInput()
+  }
+
+  const handleCheck = (index) => {
+    const newTodo = todoList.map((e,i) => {
+      if(i===index){
+        return {
+          todo: e.todo,
+          status :true
+        }
+      }else{
+        return e
+      }
+    })
+    setTodoList(newTodo)
+  }
+  
+const handleDelete = (index) => {
+  const newTodo = todoList.filter((e,i) => {
+    if (i!==index) {
+      return e
+    }
+  })
+  setTodoList(newTodo)
+}
+
+const handleEdit = (index) => {
+  const detailTodo = {
+    index,
+    ...todoList[index]
+  }
+  setForm(detailTodo)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="posisi">
+        <h1>To Do List</h1>
+        <form className="form" method="post" onSubmit={handleSubmit} >
+          <input type="text" name="todo" value={form.todo} onChange={handleChange} placeholder="Add todo..."/> 
+          <button className="btn-submit" type="submit">Submit</button>
+        </form>
+      </div>
+      <div className="content">
+          {
+            todoList.map((e,i)=>(
+              <div key={i} className="card">
+                <div className="action">
+                  <input type="checkbox" checked={e.status?true:false} onChange={()=>handleCheck(i)}/>
+                </div>
+                <div className="text">
+                    {e.todo}
+                </div>
+                <div className="button-action">
+                  <button className="btn-edit" onClick={()=>handleEdit(i)}>Edit</button>
+                  <button className="btn-delete" onClick={()=>handleDelete(i)}>Delete</button>
+                </div>
+              </div>
+            ))
+          }
+      </div>
     </div>
-  );
+    );
 }
 
 export default App;
