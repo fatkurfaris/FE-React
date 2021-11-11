976e25fc5f96d17dcebb41552e9b633204af0b04
fatkurfaris <71273351+fatkurfaris@users.noreply.github.com>
2021-11-11 13:24:56 +0700

    first commit

---
+++ task-testing/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import FormCoding from './pages/Form/FormCoding'
+import Search from './pages/Form/Search';
+
+function App() {
+  return (
+    <div className="App">
+      <FormCoding />
+      <Search />
+    </div>
+  );
+}
+
+export default App;
